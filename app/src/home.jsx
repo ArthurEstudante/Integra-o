8c0f1894e1bdfee882d1b34a60da8993e4a6bcce
ARTHUR D'EÇA MORGENSTERN VINHAS MARCHETTI <arthur_d_marchetti@estudante.sesisenai.org.br>
2024-04-23 12:51:29 -0300

    Funcionando

--- app/src/home.jsx
+++ app/src/home.jsx
@@ -3,7 +3,7 @@ import React from "react"
  function Home(){
     return(
         <>
-        <h1>HOME MANEIRO HEIN</h1>
+        <h1>HOME</h1>
         <h4><a href="/">login</a></h4>
         <h4><a href="/signin">cadastro</a></h4>
         </>
