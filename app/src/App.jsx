d2d0185193eadc0b8c8aaeb61f1267d1a430bce6
ARTHUR D'EÇA MORGENSTERN VINHAS MARCHETTI <arthur_d_marchetti@estudante.sesisenai.org.br>
2024-04-23 12:51:08 -0300

    Funcionando

---
+++ app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import  Home  from './home'
+import { Signin } from './signin';
+import Login from './login';
+export default function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+
+  )
+}
---
+++ app/src/home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+ function Home(){
+    return(
+        <>
+        <h1>HOME MANEIRO HEIN</h1>
+        <h4><a href="/">login</a></h4>
+        <h4><a href="/signin">cadastro</a></h4>
+        </>
+    )
+}
+export default Home;
